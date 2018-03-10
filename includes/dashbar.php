df0bfa18610442cc7bcf06d098a089d8fb8f286e
jjc57 <jjc57@njit.edu>
2018-03-10 13:05:30 -0500

    modified the dashbar 

--- includes/dashbar.php
+++ includes/dashbar.php
@@ -22,104 +22,49 @@
                 <!-- /input-group -->
             </li>
             <li>
-                <a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
-            </li>
-
-
-            <li>
-                <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Students<span class="fa arrow"></span></a>
+                <a href="#"><i class="fa fa-calendar fa-fw"></i> Sections<span class="fa arrow"></span></a>
                 <ul class="nav nav-second-level">
                     <li>
-                        <a href="../public/studentreg.php">Add Student</a>
+                        <a href="#">View Sections</a>
                     </li>
                     <li>
-                        <a href="morris.html">Students Reports</a>
-                    </li>
-                </ul>
-                <!-- /.nav-second-level -->
-            </li>
-
-            <li>
-                <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Coaches<span class="fa arrow"></span></a>
-                <ul class="nav nav-second-level">
-                    <li>
-                        <a href="../public/coachreg.php">Add Coaches</a>
-                    </li>
-                    <li>
-                        <a href="../public/coachreport.php">Coaches Reports</a>
-                    </li>
-                </ul>
-                <!-- /.nav-second-level -->
-            </li>
-
-            <li>
-                <a href="tables.html"><i class="fa fa-table fa-fw"></i> Tables</a>
-            </li>
-            <li>
-                <a href="forms.html"><i class="fa fa-edit fa-fw"></i> Forms</a>
-            </li>
-            <li>
-                <a href="#"><i class="fa fa-wrench fa-fw"></i> UI Elements<span class="fa arrow"></span></a>
-                <ul class="nav nav-second-level">
-                    <li>
-                        <a href="panels-wells.html">Panels and Wells</a>
-                    </li>
-                    <li>
-                        <a href="buttons.html">Buttons</a>
-                    </li>
-                    <li>
-                        <a href="notifications.html">Notifications</a>
-                    </li>
-                    <li>
-                        <a href="typography.html">Typography</a>
-                    </li>
-                    <li>
-                        <a href="icons.html"> Icons</a>
-                    </li>
-                    <li>
-                        <a href="grid.html">Grid</a>
+                        <a href="#">Edit Sections</a>
                     </li>
                 </ul>
                 <!-- /.nav-second-level -->
             </li>
             <li>
-                <a href="#"><i class="fa fa-sitemap fa-fw"></i> Multi-Level Dropdown<span class="fa arrow"></span></a>
+                <a href="#"><i class="fa fa-road fa-fw"></i> Tracks<span class="fa arrow"></span></a>
                 <ul class="nav nav-second-level">
                     <li>
-                        <a href="#">Second Level Item</a>
+                        <a href="#">View Tracks</a>
                     </li>
                     <li>
-                        <a href="#">Second Level Item</a>
-                    </li>
-                    <li>
-                        <a href="#">Third Level <span class="fa arrow"></span></a>
-                        <ul class="nav nav-third-level">
-                            <li>
-                                <a href="#">Third Level Item</a>
-                            </li>
-                            <li>
-                                <a href="#">Third Level Item</a>
-                            </li>
-                            <li>
-                                <a href="#">Third Level Item</a>
-                            </li>
-                            <li>
-                                <a href="#">Third Level Item</a>
-                            </li>
-                        </ul>
-                        <!-- /.nav-third-level -->
+                        <a href="#">Edit Tracks</a>
                     </li>
                 </ul>
                 <!-- /.nav-second-level -->
             </li>
-            <li class="active">
-                <a href="#"><i class="fa fa-files-o fa-fw"></i> Sample Pages<span class="fa arrow"></span></a>
+            <li>
+                <a href="#"><i class="fa fa-users fa-fw"></i> Coaches<span class="fa arrow"></span></a>
                 <ul class="nav nav-second-level">
                     <li>
-                        <a class="active" href="blank.html">Blank Page</a>
+                        <a href="../public/coachreg.php">View Coaches</a>
                     </li>
                     <li>
-                        <a href="login.html">Login Page</a>
+                        <a href="#">Edit Coaches</a>
+                    </li>
+                </ul>
+                <!-- /.nav-second-level -->
+            </li>
+            <li>
+                <a href="#"><i class="fa fa-graduation-cap fa-fw"></i> Students<span class="fa arrow"></span></a>
+                <ul class="nav nav-second-level">
+                    <li>
+                        <a href="#">View Students</a>
+                    </li>
+                    <li>
+                        <a href="../public/studentreg.php">Edit Students</a>
                     </li>
                 </ul>
                 <!-- /.nav-second-level -->
